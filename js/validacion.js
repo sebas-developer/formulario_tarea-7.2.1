30f02572aeed10c042097b91c54495ce18fbe6e6
marielaveron <maravsf@gmail.com>
2023-08-09 08:58:57 -0300

    Checkbox funcional

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,18 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+// Validación de los checkbox
+
+document.addEventListener("DOMContentLoaded", function(){
+    let checkbox = document.getElementById("terminos");
+    let boton = document.getElementById("regBtn");
+
+    regBtn.addEventListener("click", function(){
+        if (checkbox.boton) {
+            showAlertSuccess()
+        } else {
+            showAlertError()
+        }
+    })
+})
