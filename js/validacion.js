f7757fc30e36353f01e0bdb278245393bad016b1
Sebastian Perez <98673108+sebas-developer@users.noreply.github.com>
2023-08-09 09:06:07 -0300

    validacion de passwords, required de inputs

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,26 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.getElementById('regBtn').addEventListener('click', () => {
+
+    let inputs = document.getElementsByClassName('form-control');
+
+    if (inputs == null) {
+    showAlertError();
+    }
+
+})
+
+document.getElementById('regBtn').addEventListener('click', () => {
+
+    let password1 = document.getElementById('password1').value;
+    let password2 = document.getElementById('password2').value;
+
+    if (password1 == password2) {
+        showAlertSuccess();
+    }
+    else {
+        showAlertError();
+    }
+})
