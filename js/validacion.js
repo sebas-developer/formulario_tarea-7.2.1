12bccbc99ae9795c501f4e945a7fd5147e2ac4c1
Sebastian Perez <98673108+sebas-developer@users.noreply.github.com>
2023-08-09 09:36:36 -0300

    otro arreglo

--- js/validacion.js
+++ js/validacion.js
@@ -8,10 +8,11 @@ function showAlertError() {
 
 document.getElementById('regBtn').addEventListener('click', () => {
     let inputs = document.getElementsByClassName('form-control');
+    let check = document.getElementById('terminos').checked;
     let password1 = document.getElementById('password1').value;
     let password2 = document.getElementById('password2').value;
 
-    if (password1 !== '' && password1 === password2 && inputs.length !== 0) {
+    if (password1 !== '' && password1 === password2 && inputs.length !== 0 && check = true) {
         showAlertSuccess();
     } else {
         showAlertError();
